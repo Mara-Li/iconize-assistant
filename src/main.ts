8b80b8b437803d2e3df5214cfdea4333328a87ca
Mara <mara-li@outlook.fr>
2026-02-23 07:17:14 +0100

    chore: swap to last dev wf

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { normalizePath,Plugin, TFile, TFolder,  } from "obsidian";
-import { App } from "obsidian-undocumented";
+import { normalizePath, Plugin, TFile, TFolder } from "obsidian";
 import dedent from "ts-dedent";
 
-import { DEFAULT_SETTINGS, IconFile, IconizeAssistantSettings, Rule } from "./interface";
+import {
+	DEFAULT_SETTINGS,
+	IconFile,
+	IconizeAssistantSettings,
+	Rule,
+} from "./interface";
 import { IconizeAssistantTab } from "./settings";
 
 export default class IconizeAssistant extends Plugin {
@@ -18,14 +22,19 @@ export default class IconizeAssistant extends Plugin {
 			}
 			return result;
 		}
-		return iconPackName.charAt(0).toUpperCase() + iconPackName.charAt(1).toLowerCase();
+		return (
+			iconPackName.charAt(0).toUpperCase() +
+			iconPackName.charAt(1).toLowerCase()
+		);
 	}
 
 	searchIfApplicable(rules: Rule[], path: string): undefined | Rule {
 		if (!this.settings.allowRegex) return undefined;
 		return rules.find((rule: any) => {
 			const regex = new RegExp(rule.rule);
-			return ((rule.for === "everything" || rule.for === "files")) && regex.test(path);
+			return (
+				(rule.for === "everything" || rule.for === "files") && regex.test(path)
+			);
 		});
 	}
 
@@ -34,7 +43,7 @@ export default class IconizeAssistant extends Plugin {
 	 * 1. Inside: Same name of parent folder => "x/folder/folder.md"
 	 * 2. Outside: Same name of a folder in the same level => "x/folder.md" == "x/folder"
 	 * 3. Named index: "x/folder/index.md"
-	 * @param file 
+	 * @param file
 	 */
 	findFolderNote(file: TFile): TFolder | undefined {
 		if (file.basename === "index" || file.basename === file.parent.name) {
@@ -51,7 +60,9 @@ export default class IconizeAssistant extends Plugin {
 
 	async getFileIcons(file: TFile) {
 		//get icons folder from another obsidian plugin
-		const obsidianIconFolder = (this.app as App).plugins.getPlugin("obsidian-icon-folder");
+		const obsidianIconFolder = this.app.plugins.getPlugin(
+			"obsidian-icon-folder",
+		);
 		const data = await obsidianIconFolder.loadData();
 		const rules = data.settings.rules as Rule[];
 		const iconFolder = data.settings.iconPacksPath as string;
@@ -68,16 +79,20 @@ export default class IconizeAssistant extends Plugin {
 		 */
 		const path = file.path;
 		const isFolder = this.findFolderNote(file)?.path;
-		const fileIcon: string = icon[path] ? icon[path as string].toString() : icon[isFolder] ? icon[isFolder].toString() : null;
+		const fileIcon: string = icon[path]
+			? icon[path as string].toString()
+			: icon[isFolder]
+				? icon[isFolder].toString()
+				: null;
 		const iconPack = (await this.app.vault.adapter.list(iconFolder)).folders;
-		const allPackPrefix = iconPack.map(pack => {
+		const allPackPrefix = iconPack.map((pack) => {
 			return {
 				pack,
-				prefix: this.createIconPackPrefix(pack.split("/").pop() as string)
+				prefix: this.createIconPackPrefix(pack.split("/").pop() as string),
 			};
 		});
 		if (fileIcon) {
-			const packPrefix = allPackPrefix.find(pack => {
+			const packPrefix = allPackPrefix.find((pack) => {
 				return fileIcon.startsWith(pack.prefix);
 			});
 			if (packPrefix) {
@@ -87,7 +102,7 @@ export default class IconizeAssistant extends Plugin {
 			}
 		} else if (this.searchIfApplicable(rules, path)) {
 			const iconPath = this.searchIfApplicable(rules, path).icon;
-			const packPrefix = allPackPrefix.find(pack => {
+			const packPrefix = allPackPrefix.find((pack) => {
 				return iconPath.startsWith(pack.prefix);
 			});
 			if (packPrefix) {
@@ -100,11 +115,18 @@ export default class IconizeAssistant extends Plugin {
 	}
 
 	async editFrontmatter(file: TFile, icon: string) {
-		const getIconAsFile = this.app.vault.getAbstractFileByPath(normalizePath(`${this.settings.iconFolderPath}/${icon}.svg`));
+		const getIconAsFile = this.app.vault.getAbstractFileByPath(
+			normalizePath(`${this.settings.iconFolderPath}/${icon}.svg`),
+		);
 		console.log(`Get icon as file: ${getIconAsFile}`);
 		await this.app.fileManager.processFrontMatter(file, (frontmatter) => {
-			if (getIconAsFile && getIconAsFile instanceof TFile && this.settings.linkToFile.enable) {
-				frontmatter[`${this.settings.linkToFile.name}`] = `[[${getIconAsFile.path}|${getIconAsFile.basename}]]`;
+			if (
+				getIconAsFile &&
+				getIconAsFile instanceof TFile &&
+				this.settings.linkToFile.enable
+			) {
+				frontmatter[`${this.settings.linkToFile.name}`] =
+					`[[${getIconAsFile.path}|${getIconAsFile.basename}]]`;
 			}
 			if (this.settings.iconName.enable)
 				frontmatter[`${this.settings.iconName.name}`] = icon;
@@ -113,7 +135,9 @@ export default class IconizeAssistant extends Plugin {
 
 	async addCSS() {
 		//open styles.css or create it
-		const thisCssPath = normalizePath(`${this.app.vault.configDir}/plugins/${this.manifest.id}/styles.css`);
+		const thisCssPath = normalizePath(
+			`${this.app.vault.configDir}/plugins/${this.manifest.id}/styles.css`,
+		);
 		let css: string = "";
 		const oldCSS = await this.app.vault.adapter.read(thisCssPath);
 
@@ -147,9 +171,7 @@ export default class IconizeAssistant extends Plugin {
 	}
 
 	async onload() {
-		console.log(
-			`Iconize Assistant v.${this.manifest.version} loaded.`
-		);
+		console.log(`Iconize Assistant v.${this.manifest.version} loaded.`);
 		await this.loadSettings();
 		this.addSettingTab(new IconizeAssistantTab(this.app, this));
 		/** command that read the opened file and return the icon */
@@ -162,7 +184,7 @@ export default class IconizeAssistant extends Plugin {
 				if (file) {
 					if (!checking) {
 						const icon = await this.getFileIcons(file);
-						
+
 						if (icon) {
 							await this.editFrontmatter(file, icon);
 						}
@@ -172,12 +194,9 @@ export default class IconizeAssistant extends Plugin {
 				return false;
 			},
 		});
-
 	}
 	onunload() {
-		console.log(
-			`IconFolderYaml v.${this.manifest.version} unloaded.`
-		);
+		console.log(`IconFolderYaml v.${this.manifest.version} unloaded.`);
 	}
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
